b024751b188d0c2f3b04dc24f0c0ebcb522df67c
Karim <67035220+kmt924@users.noreply.github.com>
2023-03-08 21:00:05 +0300

    Модуль Отмены

---
+++ TPS_otmena.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import re
+
+# cmd /c cd "C:\Users\eddy1\OneDrive\Документы\Питон проекты\TP\TPS_2\TPS_03\" && C:\Users\eddy1\OneDrive\Документы\Python310\python.exe "$(FULL_CURRENT_PATH)" && pause && quit
+# && pause && quit
+# cmd /k cd "C:\Users\eddy1\OneDrive\Документы\Питон проекты\TP\TPS_2\TPS_03\" && C:\Users\eddy1\OneDrive\Документы\Python310\python.exe "$(FULL_CURRENT_PATH)" 
+def otmena():
+    df0 = pd.read_excel('ЦА_ТО_Сведения_загрузки_данных.xlsx', sheet_name='TDSheet')
+
+    column = df0.columns
+    df0_ = pd.DataFrame(columns = column)
+    df_Otmena = df0[df0['Ошибка']. str.contains
+        ('Приказ об отмене не загружен. Не найдены сведения о приказе, который требуется отменить',
+        na= False)] 
+    # Приказы об отмене
+    ##print('222 \n', df_Otmena)
+    df_Otmena_ = df_Otmena['Ошибка'].tolist()
+    ##print(len(df_Otmena_), '  - Приказы об отмене (всего)')
+    ##print(len(df0))
+    for i2 in range(len(df_Otmena_)): 
+        poz1 = df_Otmena_[i2].index(' идентификатор: ')
+        poz2 = df_Otmena_[i2].index(
+        'Приказ об отмене не загружен. Не найдены сведения о приказе, который требуется отменить')
+        ID = df_Otmena_[i2][poz1 + 16:poz2 - 3:]
+        #print(ID)
+        for i in range(len(df0)):               # Просмотр всех строк фрейма df0
+            row = df0.iloc[i]                   # строка 'Ошибка'
+            if ID == row[12]: 
+                df0_.loc[df0.index[i]] = df0.iloc[i]  # Добавляем в фрейм dff строку из df0
+                df0_.loc[df_Otmena.index[i2]] = df_Otmena.iloc[i2]
+        df0 = df0[df0.ИдентификаторОбъекта != ID]  # удаление (построчно) строк содержащих ID
+    ##print(len(df0))
+    ##print(len(df0_))
+    ##print(df0_)
+    df0_ = df0_[['НомерОбласти','Учреждение','ИдентификаторУчреждения','ЦБ',
+               'КодСВР','GUIDЗапроса','ВидФайла','ИдентификаторОбъекта','Ошибка']]
+    
+    dfff = df0_[['ВидФайла','Ошибка',]]
+    os.chdir('Отчеты')
+    if len(df0) != 0:
+        df0_.to_excel('Свод Отмененные' + '.xlsx', index=False) # Сохранение в папку Отчеты
+        #df0.to_excel('Свод без Отмен' + '.xlsx', index=False)
+    os.chdir('..')
+    return dfff
